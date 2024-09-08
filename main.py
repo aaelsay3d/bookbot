d5c7256a0f534e512cf69b20b22ca7d991f966a2
aaelsay3d <170895942+aaelsay3d@users.noreply.github.com>
2024-09-08 23:54:19 +0300

    Add sort_on function

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ def main():
     book_path = "books/frankenstein.txt"
     book_text = get_book_text(book_path)
     book_words_count = count_book_words(book_text)
+    book_characters_counter = count_characters(book_text)
 
 def get_book_text(book_path):
     with open(book_path) as f:
@@ -10,5 +11,19 @@ def get_book_text(book_path):
 def count_book_words(book_text):
     words = book_text.split()
     return len(words)
+
+def count_characters(book_text):
+    counter = {}
+    book_text = book_text.lower()
+    for char in book_text:
+        if char in counter:
+            counter[char] += 1
+        else:
+            counter[char] = 1
+    return counter
+
+def sort_on(dict):
+    key = list(dict.keys())[0]
+    return dict[key]
 
 main()
