a761c40e244644908423ce750b1aed44e5e05214
aaelsay3d <170895942+aaelsay3d@users.noreply.github.com>
2024-09-08 23:03:27 +0300

    Add count_book_words function

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 def main():
     book_path = "books/frankenstein.txt"
     book_text = get_book_text(book_path)
-
-
+    book_words_count = count_book_words(book_text)
 
 def get_book_text(book_path):
     with open(book_path) as f:
         return f.read()
+
+def count_book_words(book_text):
+    words = book_text.split()
+    return len(words)
 
 main()
